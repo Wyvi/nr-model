fbf86d935cdb5193f592cdc3a2078557f19835b0
Juraj Trappl <juraj.trappl@proton.me>
2023-11-23 13:56:33 +0100

    changed the path to correct one for the dumper ext

--- packages/oarepo-model-builder-nr/oarepo_model_builder_nr/components.py
+++ packages/oarepo-model-builder-nr/oarepo_model_builder_nr/components.py
@@ -7,7 +7,7 @@ class NrSyntheticFieldsComponent(DataTypeComponent):
 
     def before_model_prepare(self, datatype, *, context, **kwargs):
         datatype.definition["record"]["imports"].append({
-            "import": "nr_metadata.extensions.facets.dumper.SyntheticFieldsDumperExtension"
+            "import": "nr_metadata.services.records.facets.dumper.SyntheticFieldsDumperExtension"
         })
         
         datatype.definition["record-dumper"]["extensions"].append("SyntheticFieldsDumperExtension()")
