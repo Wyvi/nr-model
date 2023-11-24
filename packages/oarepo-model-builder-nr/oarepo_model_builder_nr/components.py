2edbb18ecb5fb3819bc4e5572b7cd5ff081a8770
Juraj Trappl <juraj.trappl@proton.me>
2023-11-24 08:39:00 +0100

    change ext addition

--- packages/oarepo-model-builder-nr/oarepo_model_builder_nr/components.py
+++ packages/oarepo-model-builder-nr/oarepo_model_builder_nr/components.py
@@ -6,11 +6,7 @@ class NrSyntheticFieldsComponent(DataTypeComponent):
     depends_on = [DefaultsModelComponent, RecordModelComponent, RecordDumperModelComponent]
 
     def before_model_prepare(self, datatype, *, context, **kwargs):
-        datatype.definition["record"]["imports"].append({
-            "import": "nr_metadata.services.records.facets.dumper.SyntheticFieldsDumperExtension"
-        })
-        
-        datatype.definition["record-dumper"]["extensions"].append("SyntheticFieldsDumperExtension()")
+        datatype.definition["record-dumper"]["extensions"].append("{{nr_metadata.services.records.facets.dumper.SyntheticFieldsDumperExtension}}()")
         
 COMPONENTS = [
     NrSyntheticFieldsComponent
