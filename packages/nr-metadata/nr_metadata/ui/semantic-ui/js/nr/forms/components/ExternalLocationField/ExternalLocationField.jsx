76e5f61e44458f4a81dbb37233e9654c0090dd2e
ducica <dusanstojanovic256@gmail.com>
2024-09-12 14:44:42 +0200

    external location input not visible by default

--- packages/nr-metadata/nr_metadata/ui/semantic-ui/js/nr/forms/components/ExternalLocationField/ExternalLocationField.jsx
+++ packages/nr-metadata/nr_metadata/ui/semantic-ui/js/nr/forms/components/ExternalLocationField/ExternalLocationField.jsx
@@ -1,35 +1,59 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { TextField, GroupField } from "react-invenio-forms";
-import { Form } from "semantic-ui-react";
+import { Form, Icon } from "semantic-ui-react";
 import { useFieldData } from "@js/oarepo_ui";
+import { i18next } from "@translations/nr/i18next";
+import { useFormikContext, getIn } from "formik";
 
 export const ExternalLocationField = ({ fieldPath }) => {
   const { getFieldData } = useFieldData();
+  const { values } = useFormikContext();
+  const fieldValue = getIn(values, fieldPath, {});
+
+  const hasValue = Object.values(fieldValue).some((v) => v);
+
+  const [showInput, setShowInput] = React.useState(hasValue);
 
   const { label, helpText } = getFieldData({ fieldPath });
   return (
     <Form.Field>
       {label}
-      <GroupField>
-        <TextField
-          width={8}
-          fieldPath={`${fieldPath}.externalLocationURL`}
-          {...getFieldData({
-            fieldPath: `${fieldPath}.externalLocationURL`,
-            fieldRepresentation: "compact",
-          })}
-        />
-        <TextField
-          width={8}
-          fieldPath={`${fieldPath}.externalLocationNote`}
-          {...getFieldData({
-            fieldPath: `${fieldPath}.externalLocationNote`,
-            fieldRepresentation: "compact",
-          })}
-        />
-      </GroupField>
+      {showInput && (
+        <GroupField>
+          <TextField
+            width={8}
+            fieldPath={`${fieldPath}.externalLocationURL`}
+            {...getFieldData({
+              fieldPath: `${fieldPath}.externalLocationURL`,
+              fieldRepresentation: "compact",
+            })}
+          />
+          <TextField
+            width={8}
+            fieldPath={`${fieldPath}.externalLocationNote`}
+            {...getFieldData({
+              fieldPath: `${fieldPath}.externalLocationNote`,
+              fieldRepresentation: "compact",
+            })}
+          />
+        </GroupField>
+      )}
       {helpText && <label className="helptext">{helpText}</label>}
+      {!showInput && (
+        <Form.Button
+          type="button"
+          icon
+          className={"array-field-add-button"}
+          labelPosition="left"
+          onClick={() => {
+            setShowInput(true);
+          }}
+        >
+          <Icon name="add" />
+          {i18next.t("Add external location")}
+        </Form.Button>
+      )}
     </Form.Field>
   );
 };
